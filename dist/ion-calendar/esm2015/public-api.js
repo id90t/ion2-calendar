3c5fddcaa443df2e182ead6ebf41cad9a9da9d89
MiguelBonanseaID90 <miguel.bonansea@id90travel.com>
2023-07-05 12:43:44 -0300

    [GROWTH-5752]

--- dist/ion-calendar/esm2015/public-api.js
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ion-calendar
- */
-export * from "./lib";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9wdWJsaWMtYXBpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztHQUVHO0FBRUgsY0FBYyxPQUFPLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIGlvbi1jYWxlbmRhclxuICovXG5cbmV4cG9ydCAqIGZyb20gXCIuL2xpYlwiO1xuIl19
